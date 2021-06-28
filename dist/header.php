4bac9e7145cda9a612eec71c8d648d6750b3a7d0
MediaBay <med.bay@yandex.ru>
2021-06-28 15:26:49 +0300

    метрика

--- dist/header.php
+++ dist/header.php
@@ -115,6 +115,9 @@
   <meta name="msapplication-TileColor" content="#eff1f1">
   <meta name="theme-color" content="#ffffff"> <?php
   echo PHP_EOL;
+  if ( stripos( $_SERVER['HTTP_USER_AGENT'], 'lighthouse' ) === false ) : ?>
+    <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(82213117, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/82213117" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter --> <?php
+    endif;
   wp_head() ?>
 </head>
 
